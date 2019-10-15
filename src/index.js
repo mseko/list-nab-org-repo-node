88df8a61235b633d44bb6bf8ecb6ad569eeeacd4
mseko <masaya.seko@nifty.ne.jp>
2019-10-15 20:47:46 +0900

    ネストを浅くした。

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-const {Command, flags} = require('@oclif/command')
+const {Command, flags} = require('@oclif/command');
 
 class ListOrgRepoCommand extends Command {
-
   async run() {
-    const {flags} = this.parse(ListOrgRepoCommand)
-    const orgName = "nablarch"
-    require('request')({
-        url: "https://api.github.com/orgs/" + orgName + "/repos?per_page=200&page=1",
-        headers: {
-          'User-Agent': 'node.js'
-        }
-      }, (error, response, body) => {
-        if (response.statusCode == 200) {
-          const parsed = JSON.parse(body)
-          for (let obj of parsed) {
-            console.log(obj.clone_url)
-          }
-        }
+    const {flags} = this.parse(ListOrgRepoCommand);
+    const orgName = "nablarch";
+    const perPage = 200;
+    const page = 1;
+    const requestOptions = {
+      url: "https://api.github.com/orgs/" + orgName + "/repos?per_page=" + perPage + "&page=" + page,
+      headers: {
+        'User-Agent': 'node.js'
       }
-    )
+    };
+    require('request')(requestOptions, this.requestCallback);
   }
 
+  requestCallback(error, response, body) {
+    if (response.statusCode === 200) {
+      const parsed = JSON.parse(body);
+      for (let obj of parsed) {
+        console.log(obj.clone_url);
+      }
+    } else {
+      console.error(response.statusCode);
+    }
+  }
 }
 
 ListOrgRepoCommand.description = `Describe the command here
@@ -36,4 +40,4 @@ ListOrgRepoCommand.flags = {
   name: flags.string({char: 'n', description: 'name to print'}),
 }
 
-module.exports = ListOrgRepoCommand
+module.exports = ListOrgRepoCommand;
