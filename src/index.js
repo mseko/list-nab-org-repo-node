16aa985c8e47054871785b3a62e3613e88cbc6dc
mseko <masaya.seko@nifty.ne.jp>
2019-10-19 20:57:01 +0900

    コメント追加

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 const {Command, flags} = require('@oclif/command');
 const request = require('request');
 
+/**
+ * GithubのAPIをキックし、organization配下のリポジトリのURL一覧を取得する。
+ */
 class ListOrgRepoCommand extends Command {
+  /**
+   * コマンド実行時に呼ばれる。
+   * @returns {Promise<void>}
+   */
   async run() {
+    //1ページ当たりの取得数。
     const PER_PAGE = 100;
 
+    //実行時に渡された引数及びフラグを取得
     const {args, flags} = this.parse(ListOrgRepoCommand);
+
+    // organizationがフラグで渡されていたらそれを使う。渡されていないない場合は、デフォルト値を使用する。
     let orgName = "nablarch";
     if (flags.org !== undefined) {
       orgName = flags.org;
     }
+
+    // ページ数がフラグで渡されていたらそれを使う。渡されていないない場合は、デフォルト値を使用する。
     let pages = 2;
     if (flags.pages !== undefined) {
       pages = flags.pages;
@@ -18,6 +31,7 @@ class ListOrgRepoCommand extends Command {
     try {
       for (let i = 1; i <= pages; i++) {
         let requestOptions = {
+          //GithubのAPIをキックするためのURLを組み立てる
           url: "https://api.github.com/orgs/" + orgName + "/repos?per_page=" + PER_PAGE + "&page=" + i,
           headers: {
             'User-Agent': 'node.js'
@@ -34,6 +48,11 @@ class ListOrgRepoCommand extends Command {
     }
   }
 
+  /**
+   * HTTP通信して応答を取得する。
+   * @param options 通信のオプション
+   * @returns {Promise<unknown>}
+   */
   doRequest(options) {
     return new Promise(function (resolve, reject) {
       request(options, function (error, res) {
@@ -45,7 +64,6 @@ class ListOrgRepoCommand extends Command {
       });
     });
   }
-
 }
 
 ListOrgRepoCommand.description = `Describe the command here
